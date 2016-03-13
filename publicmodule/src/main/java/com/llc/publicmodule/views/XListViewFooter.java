a368d8ab6340f951529f1bfd901150d8f2620545
liliuchen <18956247392@163.com>
2016-03-13 17:12:49 +0800

    修改部分问题

--- publicmodule/src/main/java/com/llc/publicmodule/views/XListViewFooter.java
+++ publicmodule/src/main/java/com/llc/publicmodule/views/XListViewFooter.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.llc.publicmodule.R;
 
 
 public class XListViewFooter extends LinearLayout {
